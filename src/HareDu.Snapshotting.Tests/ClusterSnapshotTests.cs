c6f93ba23d772b53edc4c03b78fe9ac3aeba0802
Albert Hives <ahives@hotmail.com>
2021-02-28 16:22:10 -0800

    refactored some code and added some extension methods for taking snapshots.

--- src/HareDu.Snapshotting.Tests/ClusterSnapshotTests.cs
+++ src/HareDu.Snapshotting.Tests/ClusterSnapshotTests.cs
@@ -22,19 +22,58 @@ namespace HareDu.Snapshotting.Tests
         }
 
         [Test]
-        public async Task Verify_can_return_snapshot()
+        public async Task Verify_can_return_snapshot1()
         {
-            var lens = _services.GetService<ISnapshotFactory>()
-                .Lens<ClusterSnapshot>();
-            var result = await lens.TakeSnapshot();
+            var result = await _services.GetService<ISnapshotFactory>()
+                .Lens<ClusterSnapshot>()
+                .TakeSnapshot();
 
             Assert.Multiple(() =>
             {
-                Assert.AreEqual(1, lens.History.Results.Count);
-                
-                lens.TakeSnapshot();
-                
-                Assert.AreEqual(2, lens.History.Results.Count);
+                Assert.IsNotNull(result);
+                Assert.IsNotNull(result.Snapshot.Nodes[0]);
+                Assert.IsNotNull(result.Snapshot.Nodes[0]?.Memory);
+                Assert.IsNotNull(result.Snapshot.Nodes[0]?.OS);
+                Assert.IsNotNull(result.Snapshot.Nodes[0]?.OS?.FileDescriptors);
+                Assert.IsNotNull(result.Snapshot.Nodes[0]?.OS?.SocketDescriptors);
+                Assert.IsNotNull(result.Snapshot.Nodes[0]?.Disk);
+                Assert.IsNotNull(result.Snapshot.Nodes[0]?.Disk?.Capacity);
+                Assert.IsNotNull(result.Snapshot.Nodes[0]?.Disk?.IO?.Writes);
+                Assert.IsNotNull(result.Snapshot.Nodes[0]?.Disk?.IO);
+                Assert.IsNotNull(result.Snapshot.Nodes[0]?.Disk?.IO?.Writes?.Bytes);
+                Assert.IsNotNull(result.Snapshot.Nodes[0]?.Disk?.IO?.Reads);
+                Assert.IsNotNull(result.Snapshot.Nodes[0]?.Disk?.IO?.Reads?.Bytes);
+                Assert.IsNotNull(result.Snapshot.Nodes[0]?.Runtime);
+                Assert.IsNotNull(result.Snapshot.Nodes[0]?.Runtime.Processes);
+                Assert.IsTrue(result.Snapshot.Nodes[0]?.Memory?.AlarmInEffect);
+                Assert.IsTrue(result.Snapshot.Nodes[0]?.Disk?.AlarmInEffect);
+                Assert.IsTrue(result.Snapshot.Nodes[0]?.IsRunning);
+                Assert.AreEqual("3.7.18", result.Snapshot.BrokerVersion);
+                Assert.AreEqual("fake_cluster", result.Snapshot.ClusterName);
+                Assert.AreEqual(723746434, result.Snapshot.Nodes[0].Memory?.Limit);
+                Assert.AreEqual("OS123", result.Snapshot.Nodes[0].OS?.ProcessId);
+                Assert.AreEqual(9203797, result.Snapshot.Nodes[0].OS?.FileDescriptors?.Used);
+                Assert.AreEqual(8298347, result.Snapshot.Nodes[0].OS?.SocketDescriptors?.Used);
+                Assert.AreEqual(7265368234, result.Snapshot.Nodes[0].Disk?.Capacity?.Available);
+                Assert.AreEqual(8928739432, result.Snapshot.Nodes[0].Disk?.Limit);
+                Assert.AreEqual(8, result.Snapshot.Nodes[0].AvailableCoresDetected);
+                Assert.AreEqual(36478608776, result.Snapshot.Nodes[0].Disk?.IO?.Writes?.Total);
+                Assert.AreEqual(728364283, result.Snapshot.Nodes[0].Disk?.IO?.Writes?.Bytes?.Total);
+                Assert.AreEqual(892793874982, result.Snapshot.Nodes[0].Disk?.IO?.Reads?.Total);
+                Assert.AreEqual(78738764, result.Snapshot.Nodes[0].Disk?.IO?.Reads?.Bytes?.Total);
+                Assert.AreEqual(9199849, result.Snapshot.Nodes[0].Runtime.Processes.Used);
+                Assert.AreEqual(new List<string>{"partition1", "partition2", "partition3", "partition4"}, result.Snapshot.Nodes[0]?.NetworkPartitions);
+            });
+        }
+
+        [Test]
+        public async Task Verify_can_return_snapshot2()
+        {
+            var result = await _services.GetService<ISnapshotFactory>()
+                .TakeClusterSnapshot();
+
+            Assert.Multiple(() =>
+            {
                 Assert.IsNotNull(result);
                 Assert.IsNotNull(result.Snapshot.Nodes[0]);
                 Assert.IsNotNull(result.Snapshot.Nodes[0]?.Memory);
--- src/HareDu.Snapshotting.Tests/ConnectionSnapshotTests.cs
+++ src/HareDu.Snapshotting.Tests/ConnectionSnapshotTests.cs
@@ -22,11 +22,47 @@ namespace HareDu.Snapshotting.Tests
         }
 
         [Test]
-        public async Task Test()
+        public async Task Verify_can_return_snapshot1()
         {
             var result = await _services.GetService<ISnapshotFactory>()
                 .Lens<BrokerConnectivitySnapshot>()
                 .TakeSnapshot();
+            
+            Assert.Multiple(() =>
+            {
+                Assert.IsNotNull(result);
+                Assert.IsNotNull(result.Snapshot);
+                Assert.IsNotNull(result.Snapshot.Connections);
+                Assert.IsNotNull(result.Snapshot.Connections[0]?.NetworkTraffic);
+                Assert.IsNotNull(result.Snapshot.Connections[0]?.NetworkTraffic?.Received);
+                Assert.IsNotNull(result.Snapshot.Connections[0]?.NetworkTraffic?.Sent);
+                Assert.IsNotNull(result.Snapshot.Connections[0]?.Channels);
+                Assert.AreEqual("3.7.18", result.Snapshot.BrokerVersion);
+                Assert.AreEqual("fake_cluster", result.Snapshot.ClusterName);
+                Assert.AreEqual("Connection 1", result.Snapshot.Connections[0]?.Identifier);
+                Assert.AreEqual(ConnectionState.Blocked, result.Snapshot.Connections[0]?.State);
+                Assert.AreEqual(982738, result.Snapshot.Connections[0]?.OpenChannelsLimit);
+                Assert.AreEqual("TestVirtualHost", result.Snapshot.Connections[0]?.VirtualHost);
+                Assert.AreEqual("Node 1", result.Snapshot.Connections[0]?.NodeIdentifier);
+                Assert.AreEqual(68721979894793, result.Snapshot.Connections[0]?.NetworkTraffic?.Received?.Total);
+                Assert.AreEqual(871998847, result.Snapshot.Connections[0]?.NetworkTraffic?.Sent?.Total);
+                Assert.AreEqual(627378937423, result.Snapshot.Connections[0]?.NetworkTraffic?.MaxFrameSize);
+                Assert.IsTrue(result.Snapshot.Connections[0]?.Channels.Any());
+                Assert.AreEqual("Channel 1", result.Snapshot.Connections[0]?.Channels[0]?.Identifier);
+                Assert.AreEqual(90, result.Snapshot.Connections[0]?.Channels[0]?.Consumers);
+                Assert.AreEqual(78, result.Snapshot.Connections[0]?.Channels[0]?.PrefetchCount);
+                Assert.AreEqual(7882003, result.Snapshot.Connections[0]?.Channels[0]?.UnacknowledgedMessages);
+                Assert.AreEqual(98237843, result.Snapshot.Connections[0]?.Channels[0]?.UncommittedAcknowledgements);
+                Assert.AreEqual(82930, result.Snapshot.Connections[0]?.Channels[0]?.UnconfirmedMessages);
+                Assert.AreEqual(383902, result.Snapshot.Connections[0]?.Channels[0]?.UncommittedMessages);
+            });
+        }
+
+        [Test]
+        public async Task Verify_can_return_snapshot2()
+        {
+            var result = await _services.GetService<ISnapshotFactory>()
+                .TakeConnectivitySnapshot();
             
             Assert.Multiple(() =>
             {
