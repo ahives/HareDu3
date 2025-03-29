2eb58db068bb1ce5478309bc1078a64b2162e87e
ahives <ahives@hotmail.com>
2025-03-28 23:15:10 -0700

    Remove obsolete tests for user connection and channel limits.
    The tests for verifying max connections and max channels were deleted as they are no longer relevant or needed. This simplifies the codebase and ensures the test suite focuses on current functionality.

--- src/HareDu.Tests/UserTests.cs
+++ src/HareDu.Tests/UserTests.cs
@@ -577,40 +577,7 @@ public class UserTests :
             Assert.AreEqual(2, result.DebugInfo.Errors.Count);
         });
     }
-
-    [Test]
-    public async Task Verify_can_get_max_connections()
-    {
-        var services = GetContainerBuilder("TestData/UserLimitsMaxConnections.json").BuildServiceProvider();
-        var result = await services.GetService<IBrokerFactory>()
-            .API<User>()
-            .GetMaxConnections("guest");
-
-        Assert.Multiple(() =>
-        {
-            Assert.IsFalse(result.HasFaulted);
-            Assert.IsTrue(result.HasData);
-            Assert.IsNotNull(result.Data);
-            Assert.AreEqual(55, result.Data.Value);
-        });
-    }
-
-    [Test]
-    public async Task Verify_can_get_max_channels()
-    {
-        var services = GetContainerBuilder("TestData/UserLimitsMaxChannels.json").BuildServiceProvider();
-        var result = await services.GetService<IBrokerFactory>()
-            .API<User>()
-            .GetMaxConnections("guest");
-
-        Assert.Multiple(() =>
-        {
-            Assert.IsFalse(result.HasFaulted);
-            Assert.IsTrue(result.HasData);
-            Assert.IsNotNull(result.Data);
-            Assert.AreEqual(50, result.Data.Value);
-        });
-    }
+    
     [Test]
     public async Task Verify_can_get_all_user_permissions1()
     {
